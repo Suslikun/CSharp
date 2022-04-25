9fc240dc1106a8cf8c349e78f79c82c2c4ecb9c0
Suslikun <bulg-sasha@inbox.ru>
2022-04-25 15:00:04 +0300

    Задания 1-6

---
+++ Lesson1/Exe/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe
+{
+    class Les1
+    {
+        public void Print(string msg)
+        {
+            int x = Console.WindowWidth / 2 - msg.Length / 2;
+            int y = Console.WindowHeight / 2;
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(msg);
+        }
+
+        public double distance(int x1, int y1, int x2, int y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+    }
+}
